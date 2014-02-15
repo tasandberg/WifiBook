4cafe9ea10cb9d73e2292e7af5778f4baa5843ca
Tim Sandberg <tasandberg@gmail.com>
2014-02-15 12:19:30 -0800

    Notification Bar added, not fully functional.  Monitor service persistance.

--- src/com/tim/wifibook/WelcomeFragment.java
+++ src/com/tim/wifibook/WelcomeFragment.java
@@ -2,10 +2,12 @@ package com.tim.wifibook;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,10 +28,8 @@ public class WelcomeFragment extends Fragment {
     private static final String TAG = "WelcomeFragment";
     public ImageView iconView;
     public ArrayList<Network> mPrefNetworks;
-    boolean shouldStartAlarm;
     Handler handler;
     WifiManager mWifiManager;
-    int fragVal;
 
     static WelcomeFragment init(int val){
         WelcomeFragment wf = new WelcomeFragment();
@@ -41,17 +41,17 @@ public class WelcomeFragment extends Fragment {
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        shouldStartAlarm = !WifiService.isServiceAlarmOn(getActivity());
+
         mPrefNetworks = NetworkManager.get(getActivity()).getNetworks();
         mWifiManager = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
-        fragVal = getArguments() != null ? getArguments().getInt("val") : 1;
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_welcome,container, false);
         final LinearLayout ll = (LinearLayout) v.findViewById(R.id.welcome_view);
-        Log.d(TAG, "onCreate in WelcomeFragment");
+        Log.d(TAG, "onCreate() WelcomeFragment");
 
         //***  Status TextView  and Updater ********//
         final TextView status_view = (TextView)ll.findViewById(R.id.status_textView);
@@ -64,6 +64,7 @@ public class WelcomeFragment extends Fragment {
             }
         });
 
+        //*** Update UI with connection status ***//
         final Runnable updateStatus = new Runnable(){
             @Override
             public void run() {
@@ -74,7 +75,7 @@ public class WelcomeFragment extends Fragment {
                     status_view.setText("Status: No connection");
                 }
                 String appender = "\n";
-                if(shouldStartAlarm) appender += "Wifi Management OFF";
+                if(serviceRunning()) appender += "Wifi Management OFF";
                 else appender += "Wifi Management ON";
                 status_view.append(appender);
                 handler.postDelayed(this,2000);
@@ -87,40 +88,32 @@ public class WelcomeFragment extends Fragment {
 
         //****** Toggle Button  *********//
         final ToggleButton on_off = (ToggleButton) v.findViewById(R.id.onOff);
-        if(!shouldStartAlarm){
-            Log.d(TAG, "Toggle button.. nothing checked");
+        if(!serviceRunning()){
             on_off.setChecked(false);
-            WifiService.setServiceAlarm(getActivity(),false);
-        }
+        } else on_off.setChecked(true);
             on_off.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(getActivity(), WifiService.class);
 
                     if(!on_off.isChecked()){
+                        //STOP SERVICE
+                        getActivity().stopService(intent);
                         Log.d(TAG,"Service stopped");
-                        WifiService.setServiceAlarm(getActivity(), false);
 
-                        if(getActivity().startService(intent) != null) {
-                            Log.d(TAG, "Cancelling again...");
-                            getActivity().stopService(intent);
-                            WifiService.setServiceAlarm(getActivity(),false);
-
-                        }
                     }
                     else{
+                        //START SERVICE
+                        getActivity().startService(intent);
+
                         Log.d(TAG,"Service started");
-                        WifiService.setServiceAlarm(getActivity(), true);
-                        shouldStartAlarm = false;
 
                     }
                     handler.post(updateStatus);
                 }
             });
 
-        if(!shouldStartAlarm) {
-            on_off.setChecked(true);
-        }
+
         return v;
     }
 
@@ -135,6 +128,13 @@ public class WelcomeFragment extends Fragment {
         Log.d(TAG, "On resume");
         super.onResume();
     }
+
+    private boolean serviceRunning() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        boolean isRunning = sp.getBoolean("running", false);
+        return isRunning;
+    }
+
 
 
     
--- src/com/tim/wifibook/WifiService.java
+++ src/com/tim/wifibook/WifiService.java
@@ -4,17 +4,19 @@ package com.tim.wifibook;
 Testing Git Commit
  */
 
-import android.app.AlarmManager;
+import android.app.Activity;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.*;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -25,37 +27,61 @@ import java.util.List;
  */
 
 public class WifiService extends Service {
+    final static int notificationID = 7278;
     private static final String TAG = "WifiService";
     private WifiManager mWifiManager;
     private List<ScanResult> result_list;
     private ArrayList<Network> mNetworks;
     private int count = 1;
-    private static final int CHECK_INTERVAL = 1000 * 20;
+    private static final int CHECK_INTERVAL = 1000 * 120; //2 minutes
 
     @Override
     public void onCreate(){
+
         Log.d(TAG, "onCreate() called in Service");
         mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        registerReceiver(mBroadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
+        readWepConfig();
+        //registerReceiver(mBroadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        registerReceiver(mBroadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("running", false);
+        editor.commit();
+        startNotification(getApplicationContext());
         mNetworks = NetworkManager.get(getApplicationContext())
                 .getNetworks();
         if(!mWifiManager.isWifiEnabled()){
             Log.d(TAG,"Enabling Wifi for scan");
             mWifiManager.setWifiEnabled(true);
         }
-        mWifiManager.startScan();
+
+        final Handler handler = new Handler();
+        Runnable scanNetworks = new Runnable(){
+            @Override
+            public void run() {
+                registerReceiver(mBroadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+                mWifiManager.startScan();
+                handler.postDelayed(this,CHECK_INTERVAL);
+            }
+        };
+        handler.post(scanNetworks);
+
         return Service.START_STICKY;
     }
+
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stopForeground(true);
+        SharedPreferences sp = getSharedPreferences("prefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("running", false);
+        editor.commit();
         Log.d(TAG,"onDestroy: unregistering receiver..");
         unregisterReceiver(mBroadcastReceiver);
     }
@@ -66,57 +92,12 @@ public class WifiService extends Service {
         return null;
     }
 
-    public static void setServiceAlarm(Context context, boolean isOn){
-        Intent i = new Intent(context, WifiService.class);
-        PendingIntent pi = PendingIntent.getService(context,0,i,0);
 
-        AlarmManager alarmManager = (AlarmManager)
-                context.getSystemService(Context.ALARM_SERVICE);
-        if(isOn){
-            alarmManager.setRepeating(AlarmManager.RTC,
-                    System.currentTimeMillis(), CHECK_INTERVAL, pi);
-        } else {
-            alarmManager.cancel(pi);
-            pi.cancel();
-        }
-    }
-
-    public boolean knownNetworksInRange(List<ScanResult> results, ArrayList<Network> myNetworks){
-        boolean preferredNetworkFound = false;
-        ArrayList<Network> preferredNetworks = new ArrayList<Network>();
-        for(Network n: myNetworks){
-            if(n.isActive()) {
-                preferredNetworks.add(n);
-            }
-        }
-        Log.d(TAG, "Working with: ");
-        for(int i = 0; i < preferredNetworks.size(); i++){
-            Log.d(TAG,"Cfg Network: " + preferredNetworks.get(i).getName());
-        }
-        for(ScanResult r : results){
-            String SSID = r.SSID;
-            Log.d(TAG, "Checking for found network: " + r.SSID);
-            for(int i = 0; i < preferredNetworks.size(); i++){
-                if(SSID.equals(preferredNetworks.get(i).getName())){
-                    Log.d(TAG,"Configured network found (" + preferredNetworks.get(i) + ")");
-                    preferredNetworkFound = true;
-                    return preferredNetworkFound;
-                }
-            }
-        }
-        Log.d(TAG,"No preferred networks found, disabling wifi");
-        return preferredNetworkFound;
-    }
 
 
     private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            readWepConfig();
-            if(!isServiceAlarmOn(getApplicationContext())){
-                Log.d(TAG,"Alarm is off, not broadcasting");
-                return;
-            }
             Log.d(TAG, "Received broadcast " + count);
             result_list = mWifiManager.getScanResults();
             count++;
@@ -126,6 +107,8 @@ public class WifiService extends Service {
             } else if(!networksInRange) {
                 mWifiManager.setWifiEnabled(false);
             }
+
+            unregisterReceiver(this);
 
         }
 
@@ -140,8 +123,7 @@ public class WifiService extends Service {
 
 
     //GATHER LIST OF ALL CONFIGURED NETWORKS
-    void readWepConfig()
-    {
+    void readWepConfig() {
         List<WifiConfiguration> items = mWifiManager.getConfiguredNetworks();
         NetworkManager.get(getApplicationContext()).getNetworks().clear();
         try{
@@ -161,6 +143,80 @@ public class WifiService extends Service {
     }
 
 
+    public boolean knownNetworksInRange(List<ScanResult> results, ArrayList<Network> myNetworks){
+        boolean preferredNetworkFound = false;
+        ArrayList<Network> preferredNetworks = new ArrayList<Network>();
+        for(Network n: myNetworks){
+            if(n.isActive()) {
+                preferredNetworks.add(n);
+            }
+        }
+        for(ScanResult r : results){
+            String SSID = r.SSID;
+            Log.d(TAG, "Checking for found network: " + r.SSID);
+            for(int i = 0; i < preferredNetworks.size(); i++){
+                if(SSID.equals(preferredNetworks.get(i).getName())){
+                    Log.d(TAG,"Configured network found (" + preferredNetworks.get(i) + ")");
+                    preferredNetworkFound = true;
+                    return preferredNetworkFound;
+                }
+            }
+        }
+        Log.d(TAG,"No preferred networks found, disabling wifi");
+        return preferredNetworkFound;
+    }
+
+    // DEFINE AND START NOTIFICATION
+    public void startNotification(Context context) {
+        Notification notification;
+        PendingIntent pendIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+
+        if((Build.VERSION.SDK_INT) >= Build.VERSION_CODES.DONUT) {
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setTicker("WifiBook").setContentTitle("TITLE").setContentText("CONTENT")
+                    .setWhen(System.currentTimeMillis()).setAutoCancel(false)
+                    .setOngoing(true).setPriority(Notification.PRIORITY_HIGH)
+                    .setContentIntent(pendIntent);
+            notification = builder.build();
+
+        } else {
+            notification = new Notification(R.drawable.elephi_icon, "WifiBook",
+                    System.currentTimeMillis());
+            notification.setLatestEventInfo(context, "Wifibook?!","WifiBook on", pendIntent);
+        }
+
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+        startForeground(notificationID, notification);
+
+    }
+
 
 
 }
+
+
+
+/*** GRAVEYARD ***/
+
+
+/* REMOVED TO CONVERT FROM ALARMMANAGER TO HANDLER USE
+    IN ORDER TO CLEAN UP START/STOP OF SERVICE
+
+    public static void setServiceAlarm(Context context, boolean isOn){
+        Intent i = new Intent(context, WifiService.class);
+        PendingIntent pi = PendingIntent.getService(context,0,i,0);
+
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+        if(isOn){
+            alarmManager.setRepeating(AlarmManager.RTC,
+                    System.currentTimeMillis(), CHECK_INTERVAL, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+    }
+*/
